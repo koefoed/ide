39506d9acf15bd7cb8de756c9afbd5b3628cd4f7
Koefoed <koefoed@users.noreply.github.com>
2016-01-19 18:22:53 +0100

    Insert automatic references before any text.

--- references.js
+++ references.js
@@ -1,28 +1,14 @@
 
-// Save me from typing all the reference links and numbers by inserting them programmatically
+// Save me from typing all the reference links and numbers by inserting them programmatically (before any text in the span element)
 
 function refElem(text, link) {
 	return (function(t,l){
 		return function() {
-			e = document.createElement('a');
-			e.innerHTML=t;
-			e.setAttribute('href', l);
-			return e;
+			var myText = '<a href="' + l + '">' + t + '</a>' + (this.childNodes.length == 0? '': ' '); // add a space if it was non-empty
+			this.insertAdjacentHTML('afterbegin', myText);
 		};
 	})(text,link)
 };
 
-d3.selectAll(".reference.isdals").insert( refElem('[1]', "#cite_isdals"), ":first-child" );
-d3.selectAll(".reference.speizer79").insert( refElem('[2]', "#cite_speizer1979"), ":first-child" );
-
-//~ function(){var t = document.createElement('text'); t.innerHTML='hej'; this.parentNode.insertBefore(t, this.nextSibling);}
-
-
-//~ function rE(t,l){
-	//~ return function() {
-		//~ e = document.createElement('a');
-		//~ e.innerHTML=t;
-		//~ e.setAttribute('href', l);
-		//~ this.insertBefore(e, this.childNodes[0]);
-	//~ }
-//~ };
+d3.selectAll(".reference.isdals").each( refElem('[1]', "#cite_isdals") );
+d3.selectAll(".reference.speizer79").each( refElem('[2]', "#cite_speizer1979") );
